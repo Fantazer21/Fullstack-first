d9b8590d7f303275bc55789721ed67ab96622fcd
Ilya Stepanov <6967221@gmail.com>
2022-12-01 21:34:13 +0300

    add rtk easy

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,17 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/home.module.scss'
-import {Provider, useSelector} from "react-redux";
-import {RootState, store} from "../store/store";
+import {useSelector} from "react-redux";
+import {RootState} from "../store/store";
 
 export default function Home() {
     const count = useSelector((state: RootState) => state.counter.value)
     console.log(count)
   return (
-      <Provider store={store}>
           <div className={styles.container}>
               <div> Hello world</div>
           </div>
-      </Provider>
   )
 }
